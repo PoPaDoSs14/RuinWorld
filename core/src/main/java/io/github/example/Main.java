4b6a14a59006f82549ab4c9c0abdf9a95193e542
PoPaDoSs14 <slasevvladimir7@gmail.com>
2025-01-01 16:47:30 +0300

    add input

--- core/src/main/java/io/github/example/Main.java
+++ core/src/main/java/io/github/example/Main.java
@@ -2,28 +2,34 @@ package io.github.example;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Main extends ApplicationAdapter {
     private SpriteBatch batch;
+    private Sprite sprite;
     private Texture image;
 
     @Override
     public void create() {
         batch = new SpriteBatch();
         image = new Texture("human.png");
+        sprite = new Sprite(image);
     }
 
     @Override
     public void render() {
         ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
         batch.begin();
-        batch.draw(image, 140, 210);
+        input();
+        sprite.setSize(50f, 60f);
+        sprite.draw(batch);
         batch.end();
     }
 
@@ -32,4 +38,14 @@ public class Main extends ApplicationAdapter {
         batch.dispose();
         image.dispose();
     }
+
+    private void input() {
+        float speed = 40f;
+        float delta = Gdx.graphics.getDeltaTime();
+
+        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+            sprite.translateX(speed * delta);
+        }
+    }
+
 }
