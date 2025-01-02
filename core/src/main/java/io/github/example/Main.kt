6f7cc6d8a63f175c1251c578f69c3f5f92354b83
PoPaDoSs14 <slasevvladimir7@gmail.com>
2025-01-02 14:39:43 +0300

    rework on kotlin

--- core/src/main/java/io/github/example/Main.kt
+++ core/src/main/java/io/github/example/Main.kt
@@ -1,51 +1,45 @@
-package io.github.example;
+package io.github.example
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
-import com.badlogic.gdx.Screen;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.Sprite;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.ApplicationAdapter
+import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.Input
+import com.badlogic.gdx.graphics.Texture
+import com.badlogic.gdx.graphics.g2d.Sprite
+import com.badlogic.gdx.graphics.g2d.SpriteBatch
+import com.badlogic.gdx.utils.ScreenUtils
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
-public class Main extends ApplicationAdapter {
-    private SpriteBatch batch;
-    private Sprite sprite;
-    private Texture image;
+class Main : ApplicationAdapter() {
+    private lateinit var batch: SpriteBatch
+    private lateinit var sprite: Sprite
+    private lateinit var image: Texture
 
-    @Override
-    public void create() {
-        batch = new SpriteBatch();
-        image = new Texture("human.png");
-        sprite = new Sprite(image);
+    override fun create() {
+        batch = SpriteBatch()
+        image = Texture("human.png")
+        sprite = Sprite(image)
     }
 
-    @Override
-    public void render() {
-        ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
-        batch.begin();
-        input();
-        sprite.setSize(50f, 60f);
-        sprite.draw(batch);
-        batch.end();
+    override fun render() {
+        ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f)
+        batch.begin()
+        handleInput()
+        sprite.setSize(50f, 60f)
+        sprite.draw(batch)
+        batch.end()
     }
 
-    @Override
-    public void dispose() {
-        batch.dispose();
-        image.dispose();
+    override fun dispose() {
+        batch.dispose()
+        image.dispose()
     }
 
-    private void input() {
-        float speed = 40f;
-        float delta = Gdx.graphics.getDeltaTime();
+    private fun handleInput() {
+        val speed = 40f
+        val delta = Gdx.graphics.deltaTime
 
         if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
-            sprite.translateX(speed * delta);
+            sprite.translateX(speed * delta)
         }
     }
-
 }
